e4e587724df5b0c3efed6312bbbcba39cc43a409
khetan15 <aakanksha.khetan@gmail.com>
2020-10-05 16:40:17 +0530

    video+msgupdate

--- lib/NextScreen.dart
+++ lib/NextScreen.dart
@@ -1,46 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:jerry/VideoPlayerScreen.dart';
 
 class NextScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-
       home: DefaultTabController(
-
         length: 3,
-
         child: Scaffold(
-          backgroundColor:
-
-          Colors.white,
-
+          backgroundColor: Colors.white,
           appBar: AppBar(
             backgroundColor: Color(0xff13195b),
 
             bottom: TabBar(tabs: [
-
-              Tab(text: "Dilkibaat",),
-
-              Tab(text: "Memories",),
-              Tab(text:"Tohfa"),
+              Tab(
+                text: "Dilkibaat",
+              ),
+              Tab(
+                text: "Memories",
+              ),
+              Tab(text: "Tohfa"),
             ]),
 
             centerTitle: true,
 
-            title: Text("Jerry Ka Bday", style: TextStyle(
-
-              fontWeight: FontWeight.bold,
-
-              color: Colors.white70,
-            ),), // Text Style, Text
+            title: Text(
+              "Jerry Ka Bday",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
+            ), // Text Style, Text
           ),
           body: TabBarView(
             children: [
-              Padding(padding: EdgeInsets.only(
-                  left: 16, top: 36, right: 16, bottom: 36),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 16, top: 36, right: 16, bottom: 36),
                 child: Card(
                   elevation: 10,
                   shadowColor: Colors.blueAccent,
@@ -50,58 +51,75 @@ class NextScreen extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text("heyyy",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
 
-                          ),)
+                        Text(
+                          "Jerrryy...kya likhe...u know everything..Jab first time tutorial me tmhare bagal me baithe the toh kabhi imagine nai kie the ki aage chal ke aisi dosti ho jaegi!Tareef krne layak toh kuch hai nai bas itna kahenge ki tmhara presence hi peace hai meri monk baba:* I m & will alwayssss be there for u & with you..chahe park ke chakkar katne ho ya doctor k...RR krna ho ya ghanto shopping...fruits mangwana ho ya amul se momos :P Tum bas mujhpe kitkat ki barish krte rho hm kachoriyon ki karte rhnge XD Fir aur kisi ki zaroorat hi kahan hai :) Bas itna hi hai tmse kehna<3 Happiest birthday meri strongest,cutest Chutiya!!!! "
+                              ,
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
                       ],
                     ),
                   ),
                 ),
               ),
-              Padding(padding: EdgeInsets.only(
-                  left: 16, top: 36, right: 16, bottom: 36),
-                child: Card(
-                  elevation: 10,
-                  shadowColor: Colors.blueAccent,
-                  child: ListView(
-                   children: [
-                     Image(image:AssetImage("images/10.jpeg")),
-                     Image(image:AssetImage("images/2.jpeg")),
-                     Image(image:AssetImage("images/3.jpeg")),
-                     Image(image:AssetImage("images/6.jpeg")),
-                     Image(image:AssetImage("images/7.jpeg")),
-                     Image(image:AssetImage("images/8.jpeg")),
-                     Image(image:AssetImage("images/9.jpeg")),
-                     Image(image:AssetImage("images/1.jpeg")),
-                     Image(image:AssetImage("images/5..jpeg")),
-                     Image(image:AssetImage("images/4.jpeg")),
-                   ],
-
-                    ),
-                  ),
-                ),
-              Padding(padding: EdgeInsets.only(
-                  left: 16, top: 36, right: 16, bottom: 36),
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 16, top: 36, right: 16, bottom: 36),
                 child: Card(
                   elevation: 10,
                   shadowColor: Colors.blueAccent,
                   child: ListView(
                     children: [
-                      Image(image:AssetImage("images/jerry1."))
+                      Image(image: AssetImage("images/10.jpeg")),
+                      Image(image: AssetImage("images/2.jpeg")),
+                      Image(image: AssetImage("images/3.jpeg")),
+                      Image(image: AssetImage("images/6.jpeg")),
+                      Image(image: AssetImage("images/7.jpeg")),
+                      Image(image: AssetImage("images/8.jpeg")),
+                      Image(image: AssetImage("images/9.jpeg")),
+                      Image(image: AssetImage("images/1.jpeg")),
+                      Image(image: AssetImage("images/5..jpeg")),
+                      Image(image: AssetImage("images/4.jpeg")),
                     ],
-
                   ),
                 ),
               ),
-
-
+              Padding(
+                padding:
+                    EdgeInsets.only(left: 16, top: 36, right: 16, bottom: 36),
+                    child:
+                        //Text(
+                          //"heyyy",
+                          //style: TextStyle(
+                            //fontSize: 20,
+                            //fontWeight: FontWeight.bold,
+                          //),
+                        //),
+                        Scaffold(
+                          floatingActionButton: FloatingActionButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => VideoPlayerScreen(),
+                                  ));
+                            },
+                            child: Icon(
+                              FlutterIcons.play_circle_outline_mdi,
+                              color: Colors.white,
+                              size: 55,
+                            ),
+                            backgroundColor: Color(0xFF13195b),
+                          ),
+                        )
+                  ),
             ],
           ),
-        ),),
-
+        ),
+      ),
     );
   }
 }
