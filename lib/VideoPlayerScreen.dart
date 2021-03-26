b5dfcac95692ea22925ef71d48a5fcfff5508af5
khetan15 <aakanksha.khetan@gmail.com>
2021-03-26 23:58:47 +0530

    edits

--- lib/VideoPlayerScreen.dart
+++ lib/VideoPlayerScreen.dart
@@ -27,7 +27,7 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Jerryyy To My Akkaa'),
+        title: Text('Jerryyy To My  Akkaa'),
       ),
       body: FutureBuilder(
         future: _initializeVideoPlayerFuture,
